ecd7c0a5b42900e8a9f1efddd1713ed264bf6385
Sanjana Afrin <94892759+sanjana54@users.noreply.github.com>
2023-08-09 00:08:26 +0600

    Added Creational Design Pattern

---
+++ creational_design_patterns/abstract_factory/concrete_factory/Asteriods/AsteriodsFactory.java
@@ -0,0 +1,7 @@
+package creational_design_patterns.abstract_factory.concrete_factory.Asteriods;
+
+import creational_design_patterns.abstract_factory.abstract_factory.ObstacleFactory;
+
+public abstract class AsteriodsFactory extends ObstacleFactory {
+
+}
